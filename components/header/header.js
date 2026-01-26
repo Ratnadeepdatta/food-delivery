058d04c410f22311d3a3d07ef426e723d98b5e51
Ratnadeep Datta <ratnadeepdatta19996@gmail.com>
2026-01-26 22:48:45 +0530

    Improved header

--- components/header/header.js
+++ components/header/header.js
@@ -1,1 +1,11 @@
- 
+window.addEventListener("scroll", () =>{
+const header = document.getElementById("header");
+
+if(scrollY>20){
+    header.classList.add("scrolled")
+}
+else{
+    header.classList.remove("scrolled")
+}
+
+});
