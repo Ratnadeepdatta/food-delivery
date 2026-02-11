436cdd624579e0421197e273459f761b66b7632a
Ratnadeep Datta <ratnadeepdatta1996@gmail.com>
2026-02-11 23:35:50 +0530

    Improve code

--- components/header/header.js
+++ components/header/header.js
@@ -1,11 +1,36 @@
-window.addEventListener("scroll", () =>{
-const header = document.getElementById("header");
 
-if(scrollY>20){
-    header.classList.add("scrolled")
-}
-else{
-    header.classList.remove("scrolled")
-}
+// const header= document.getElementById("header");
+// window.addEventListener('scroll', () => {
+//     if(window.scrollY > 100){
+//         header.classList.add('scrolled');
+//     }
+//     else{
+//         header.classList.remove('scrolled');
+//     }
+// });
 
+function initHeaderScroll() {
+  const header = document.querySelector(".header");
+
+  if (!header) return;
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 20) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+}
+    initHeaderScroll();
+
+
+
+// mobile menu
+const menuToggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav");
+
+menuToggle.addEventListener('click', () =>{
+    menuToggle.classList.toggle('active');
+    nav.classList.toggle('open');
 });
